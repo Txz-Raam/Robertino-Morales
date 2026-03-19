d980e774a9b77f2bc4f3451bf81ff66dab582a45
Robertino Morales <164657612+Txz-Raam@users.noreply.github.com>
2026-03-19 18:11:44 -0300

    Add files via upload

---
+++ programacion/ii/ProgramacionII.java
@@ -0,0 +1,54 @@
+package programacion.ii;
+
+import java.util.Scanner;
+
+public class ProgramacionII {
+  //ACTIVIAD 1 :
+  //  Se solicita crear una clase llamada DetallePersona que realice   lo siguiente:
+  //Debe declarar las siguientes variables y deberás asignar los valores que correspondan, además de seleccionar el tipo de dato adecuado para cada variable.
+  //>nombreCompleto
+  //>edad
+  //>altura (En metros)
+  //>paisDeOrigen
+  //>Indicar si es casado o soltero con sólo un caracter, ejemplo: 
+  //‘C’ -> casado, ‘S’ -> soltero
+  //
+    public static void main(String[] args) {
+        /*String nombrecompleto = "Robertino Alberto Morales";
+        double altura = 1.75;
+        int edad = 17;
+        String paisOrigen = "Argentina";
+        String estadoCivil = "C";
+        
+        System.out.println("Nombre: "+ nombrecompleto);
+        System.out.println("Edad: "+ edad);
+        System.out.println("Altura: "+ altura);
+        System.out.println("Pais: "+ paisOrigen);
+        System.out.println("Estado civil: "+ estadoCivil);
+        */
+        /*
+        String primerNombre = "Robertino";
+        String segundoNombre = "Alberto";
+        String Apellido = "Morales";
+        String empresa = "Cen Tech";
+        String dominio = "CEN.TECH.COM.AR";
+        
+        
+        System.out.println("****Generador de Gmail****");
+        System.out.println("Nombre completo: "+primerNombre+" "+segundoNombre+" "+Apellido);
+        System.out.println("Nombre Empresa: "+ empresa);
+        System.out.println("nombre Dominio: "+ dominio);
+        
+        System.out.println("Generando email........");
+        
+        System.out.println(primerNombre+"_"+segundoNombre+"_"+Apellido+"@"+dominio);
+        */
+        
+    
+        Scanner empleando = new Scanner(System.in);
+        System.out.println("Nombre del empleado:");
+        
+        
+    }
+    
+}
